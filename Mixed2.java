cfd99c92b6d54621c3088f2dc57b6b6a343e1e74
KhadeejaArshadAli <100237963+KhadeejaArshadAli@users.noreply.github.com>
2022-09-11 20:27:10 +0500

    Add files via upload

---
+++ Mixed2.java
@@ -0,0 +1,45 @@
+ class A {
+  int ivar=7;
+  void m1(){
+      System.out.println("A's m1, ");
+  }  
+  void m2(){
+    System.out.println("A's m2, ");
+  }
+  void m3(){
+    System.out.println("A's m3, ");
+  }
+}
+class B extends A{
+    void m1 (){
+        System.out.println("B's m1, ");
+    }
+
+}
+class C extends B {
+    void m3(){
+        System.out.println("C's m3, " +(ivar + 6));
+    }
+}
+public class Mixed2 {
+    public static void main(String[] args){
+        A a=new A();
+        B b=new B();
+        C c=new C();
+        A a2=new C();
+        b.m1();
+        c.m2();
+        a.m3();
+        c.m1();
+        c.m2();
+        c.m3();
+        a.m1();
+        b.m2();
+        c.m3();
+        a2.m1();
+        a2.m2();
+        a2.m3();
+
+
+    }
+}
